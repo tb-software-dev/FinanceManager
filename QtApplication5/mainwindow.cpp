f29faf1ca769f944f7dd2d686a535b6bbcc0beaa
tobia <tobia@DESKTOP-T2UKARU>
2025-02-26 13:31:29 +0100

    #

--- QtApplication5/mainwindow.cpp
+++ QtApplication5/mainwindow.cpp
@@ -59,5 +59,100 @@ void MainWindow::setupTable()
 	model->setColumnCount(5);
 	model->setHorizontalHeaderLabels({ "Datum", "Beschreibung", "Kategorie", "Betrag", "Typ" });
 	ui->transactionTable->setModel(model);
+	ui->transactionTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+}
+
+void MainWindow::loadTransactions()
+{
+	model->removeRows(0, model->rowCount());
+
+	QSqlQuery query("SELECT date, description, category, amount, type FROM transactions");
+
+	while (query.next())
+	{
+		QList<QStandardItem*> items;
+
+		for (int i = 0; i < 5; i++)
+		{
+			items.append(new QStandardItem(query.value(i).toString()));
+		}
+		model->appendRow(items);
+	}
+	updateTotalLabels();
+}
+
+void MainWindow::on_addButton_clicked()
+{
+	QSqlQuery query;
+	query.prepare("INSERT INTO transactions (date, description, category, amount, type )"
+		"VALUES (?, ?, ?, ?, ?)");
+
+	query.addBindValue(ui->dateEdit->date().toString("yyyy-MM-dd"));
+	query.addBindValue(ui->descriptionEdit->text());
+	query.addBindValue(ui->categoryCombo->currentText());
+	query.addBindValue(ui->amountSpin->value());
+	query.addBindValue(ui->typeCombo->currentText());
+
+	if (query.exec())
+	{
+		loadTransactions();
+		clearInputFields();
+	}
+	else
+	{
+		QMessageBox::critical(this, "Fehler", "Eintrag konnte nicht hinzugefügt werden!");
+	}
+}
+
+void MainWindow::on_editButton_clicked()
+{
+	QModelIndexList selection = ui->transactionTable->selectionModel()->selectedRows();
+
+	if (selection.isEmpty())
+	{
+		return;
+	}
+
+	int row = selection.first().row();
+
+	QSqlQuery query;
+	query.prepare("UPDATE transactions SET date=?, description=?, category=?, amount=?, type=? "
+		"WHERE rowid=?");
+	query.addBindValue(ui->dateEdit->date().toString("yyyy-MM-dd"));
+	query.addBindValue(ui->descriptionEdit->text());
+	query.addBindValue(ui->categoryCombo->currentText());
+	query.addBindValue(ui->amountSpin->value());
+	query.addBindValue(ui->typeCombo->currentText());
+	query.addBindValue(model->item(row, 0)->data(Qt::UserRole + 1)); // ID
+
+	if (query.exec())
+	{
+		loadTransactions();
+		clearInputFields();
+	}
+
+
+}
+
+void MainWindow::on_deleteButton_clicked()
+{
+	QModelIndexList selection = ui->transactionTable->selectionModel()->selectedRows();
+	if (selection.isEmpty())
+	{
+		return;
+	}
+
+	int row = selection.first().row();
+	QString id = model->item(row, 0)->data(Qt::UserRole + 1).toString();
+
+	QSqlQuery query;
+	query.prepare("DELETE FROM transactions WHERE rowid=?");
+	query.addBindValue(id);
+
+	if (query.exec())
+	{
+		loadTransactions();
+		clearInputFields();
+	}
 }
 
