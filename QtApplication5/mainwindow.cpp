def7fb6c98aff936f0f6acdff66b20d9031c637e
tobia <tobia@DESKTOP-T2UKARU>
2025-02-26 12:13:13 +0100

    #

--- QtApplication5/mainwindow.cpp
+++ QtApplication5/mainwindow.cpp
@@ -26,6 +26,38 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
 
 MainWindow::~MainWindow()
 {
+	db.close();
 	delete ui;
+}
+
+void MainWindow::initializeDatabase()
+{
+	db = QSqlDatabase::addDatabase("QSQLITE");
+	db.setDatabaseName("haushaltsbuch.db");
+
+	if (!db.open())
+	{
+		QMessageBox::critical(this, "Fehler", "Datenbank konnte nicht geöffnet werden!");
+		return;
+	}
+
+	// Tabelle erstellen
+	QSqlQuery query;
+	query.exec("CREATE TABLE IF NOT EXISTS transactions ("
+		"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		"date DATE NOT NULL,"
+		"description TEXT,"
+		"category TEXT ,"
+		"amount REAL ,"
+		"type TEXT)");
+		
+}
+
+void MainWindow::setupTable()
+{
+	model = new QStandardItemModel(this);
+	model->setColumnCount(5);
+	model->setHorizontalHeaderLabels({ "Datum", "Beschreibung", "Kategorie", "Betrag", "Typ" });
+	ui->transactionTable->setModel(model);
 }
 
